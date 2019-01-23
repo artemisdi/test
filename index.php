9a88f846d56a64ca30e194e1b2d3c78f393ea310
Kirill <golimarartemisdi@gmail.com>
2019-01-23 17:13:28 +0300

    test

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php 
-$x = 12;
-echo $x;
- ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div class="img"></div>
+	<h2>Привет тебе от пухли</h2>
+	<span></span>
+
+	<style>
+		.img {
+			background-image:url('https://pbs.twimg.com/profile_images/723919849664188417/XMghofG1_400x400.jpg');
+			widdth:200px;
+			height:200px;
+		}
+	</style>
+</body>
+</html>
